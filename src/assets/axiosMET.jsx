2c3dbd4a48a1ef050f036eb2afa35491a9b33065
Stanley <27.stanleylch@gmail.com>
2024-05-30 22:30:34 +0100

    axios fetch requests for departments and objects with images

--- src/assets/axiosMET.jsx
+++ src/assets/axiosMET.jsx
@@ -15,8 +15,34 @@ export const fetchDepartments = (department) => {
     });
 };
 
-// export const fetchEachDepartment = (departmentId) => {
-//   return request.get(`./https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${departmentId}&q=cat`);
-// };
+export const fetchSingleDepartment = (departmentId) => {
+  console.log(departmentId);
+  return request
+    .get(`/objects?departmentIds=${departmentId}&hasImages=true&q=`)
+    .then((response) => {
+      console.log(response, "response here");
+      return response;
+    })
+    .catch((err) => {
+      console.log("error fetching single department, error:", err);
+    });
+};
+
+export const fetchObjectsById = (objectId) => {
+  console.log(objectId);
+  return request
+    .get(`/objects/${objectId}`)
+    .then((response) => {
+      console.log(response, "objects response");
+      return response;
+    })
+    .catch((err) => {
+      console.log("error fetching object, error:", err);
+    });
+};
 
 // https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=6&q=cat
+
+// https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=4&hasImages=true&q=
+
+//https://collectionapi.metmuseum.org/public/collection/v1/objects/22067
