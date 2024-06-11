b40254d4479c5a092d9a9358b7fc59e23fae9bfc
Stanley <27.stanleylch@gmail.com>
2024-06-12 00:54:36 +0100

    search bar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log("Searching for:", query);
+  };
+
+  return (
+    <div className="flex items-center justify-center mt-5">
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Search..."
+        className="px-4 py-2 text-lg border border-gray-300 rounded mr-2 w-72"
+      />
+      <button
+        onClick={handleSearch}
+        className="px-4 py-2 text-lg text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none"
+      >
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/screens/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Currently on About</h1>
-    </div>
-  );
-}
